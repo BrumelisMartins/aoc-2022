5e63b468512b945678941485f6d3eeb355993aa6
brume <brumelis.martins@gmail.com>
2022-12-01 10:20:43 +0200

    First day implementation

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,37 @@
 fun main() {
+
+    fun getListOfCalories(input: List<String>): List<Int>{
+        val listOfSums = arrayListOf<Int>()
+        var sum = 0
+        input.forEach {
+            if (it.isBlank()){
+                listOfSums.add(sum)
+                sum = 0
+                return@forEach
+            }
+            sum += it.toInt()
+        }
+        //in case last element is not an empty line and was not added
+        if (sum != 0){
+            listOfSums.add(sum)
+        }
+        return listOfSums
+    }
+
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val listOfSums = getListOfCalories(input)
+        return listOfSums.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val listOfSums = getListOfCalories(input).sortedByDescending { it }
+        return listOfSums.take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
