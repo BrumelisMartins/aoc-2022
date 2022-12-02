ff646afbbca6cb9083a7363052f5f45636f9df91
brume <brumelis.martins@gmail.com>
2022-12-02 20:24:43 +0200

    Some minor improvements

--- src/Day02.kt
+++ src/Day02.kt
@@ -17,79 +17,14 @@ fun main() {
      *
      */
 
-    fun getMatchScore(elfGesture: Gesture, myGesture: Gesture): Int {
-        return if (myGesture.betterThan == elfGesture.id) {
-            6
-        } else if (myGesture == elfGesture){
-            3
-        } else {
-            0
-        }
-    }
-    val gestureList = Gesture.values()
-
-    fun toGesture(string: String): Gesture? {
-        return when (string) {
-            "A" -> Gesture.ROCK
-            "B" -> Gesture.PAPER
-            "C" -> Gesture.SCISSORS
-            "X" -> Gesture.ROCK
-            "Y" -> Gesture.PAPER
-            "Z" -> Gesture.SCISSORS
-            else -> null
-
-        }
-    }
-
-    fun getTotalScore(input: List<String>): Int {
-        val listOfGestures = input.map{ raw -> raw.split(" ").map { toGesture(it) }  }
-        var sum = 0
-        listOfGestures.forEach {
-            val elfGesture = it.first()
-            val myGesture = it.last()
-            if (elfGesture == null || myGesture == null) return@forEach
-            sum += getMatchScore(elfGesture, myGesture) + myGesture.score
-        }
-        return sum
-    }
-
-    fun getFixedMatchScore(elfGesture: Gesture, myGesture: String): Int {
-        return when (myGesture) {
-            "X" -> {
-                //lose
-                val losingGesture = gestureList.find { it.worseThan == elfGesture.id }
-                losingGesture!!.score
-            }
-            "Y" -> {
-                //draw
-                3 + elfGesture.score
-            }
-            else -> {
-                //win
-                val winingGesture = gestureList.find { it.betterThan == elfGesture.id }
-                winingGesture!!.score + 6
-            }
-        }
-    }
-
-    fun getFixedTotalResults(input: List<String>): Int {
-        val listOfGestures = input.map{ raw -> raw.split(" ") }
-        var sum = 0
-        listOfGestures.forEach {
-            val elfGesture = toGesture(it.first())
-            val myGesture = it.last()
-            if (elfGesture == null) return@forEach
-            sum += getFixedMatchScore(elfGesture, myGesture)
-        }
-        return sum
-    }
-
     fun part1(input: List<String>): Int {
-        return getTotalScore(input)
+        val convertedList = convertStringListToGesturePairs(input, false)
+        return getTotalScore(convertedList)
     }
 
     fun part2(input: List<String>): Int {
-        return getFixedTotalResults(input)
+        val convertedList = convertStringListToGesturePairs(input, true)
+        return getTotalScore(convertedList)
     }
 
     // test if implementation meets criteria from the description, like:
@@ -100,8 +35,76 @@ fun main() {
     println(part1(input))
     println(part2(input))
 }
+val gestureList = Gesture.values()
 
-enum class Gesture(val score: Int,val  id: String,val  betterThan: String,val  worseThan: String) {
+fun getMatchScore(elfGesture: Gesture, myGesture: Gesture): Int {
+    return if (myGesture.betterThan == elfGesture.id) {
+        6
+    } else if (myGesture == elfGesture) {
+        3
+    } else {
+        0
+    }
+}
+
+fun String.toGestureFromCode(elfGesture: Gesture?): Gesture? {
+    return when (this) {
+        "X" -> {
+            //lose
+            gestureList.find { it.worseThan == elfGesture?.id }
+        }
+        "Z" -> {
+            //win
+            gestureList.find { it.betterThan == elfGesture?.id }
+        }
+        else -> {
+            //draw
+            elfGesture
+        }
+    }
+}
+
+fun String.toGesture(): Gesture? {
+    return when (this) {
+        "A" -> Gesture.ROCK
+        "B" -> Gesture.PAPER
+        "C" -> Gesture.SCISSORS
+        "X" -> Gesture.ROCK
+        "Y" -> Gesture.PAPER
+        "Z" -> Gesture.SCISSORS
+        else -> null
+    }
+}
+
+fun getTotalScore(listOfGestures: List<Pair<Gesture?, Gesture?>>): Int {
+    var sum = 0
+    listOfGestures.forEach {
+        val elfGesture = it.first
+        val myGesture = it.second
+        if (elfGesture == null || myGesture == null) return@forEach
+        sum += getMatchScore(elfGesture, myGesture) + myGesture.score
+    }
+    return sum
+}
+
+fun convertStringListToGesturePairs(
+    rawList: List<String>,
+    useSecretElfCode: Boolean
+): List<Pair<Gesture?, Gesture?>> {
+    val listOfGestures = rawList.map { raw ->
+        val newList = raw.split(" ")
+        val elfGesture = newList.first().toGesture()
+        val myGesture = if (useSecretElfCode) {
+            newList.last().toGestureFromCode(elfGesture)
+        } else {
+            newList.last().toGesture()
+        }
+        Pair(elfGesture, myGesture)
+    }
+    return listOfGestures
+}
+
+enum class Gesture(val score: Int, val id: String, val betterThan: String, val worseThan: String) {
     ROCK(
         score = 1,
         id = "ROCK",
